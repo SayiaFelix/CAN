04f058594fdfc72c1cb51c7d5b0aaa0b2cd7c194
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-20 15:14:54 +0300

    adding files

--- community/models.py
+++ community/models.py
@@ -1,28 +1,53 @@
+from unicodedata import category
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-
+Service=(
+    ('Driver', 'Driver'),
+    ('Doctor', 'Doctor'),
+    ('Cleaner', 'Cleaner'),
+    ('Teacher', 'Teacher'),
+    ('Sponsor', 'Sponsor'),
+    ('Motivator', 'Motivator'),
+)
+Category=(
+    ('Sell', 'Sell'),
+    ('Donation', 'Donation'),
+  
+)
+Target=(
+    ('Kids', 'Kids'),  
+    ('Youth', 'Youth'),
+    ('Couples', 'Couples'),
+    ('Singles', 'Singles'),
+    ('Aged Groups', 'Aged Groups'),
+  
+)
 
 # Create your models here.
 class Motivation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     motiv_photo = models.ImageField(upload_to='motive/')
     motivation = HTMLField()
-    target = models.CharField(max_length=100)
-    post_date = models.DateTimeField(auto_now_add=True)
-
+    target = models.CharField(max_length=15, choices=Target, default="kids")
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
-class Business(models.Model):
-    b_photo = models.ImageField(upload_to='bussiness/')
+class Cloth(models.Model):
+    c_photo = models.ImageField(upload_to='cloth/')
     description = HTMLField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.IntegerField()
     name = models.CharField(max_length=100)
+    category = models.CharField(max_length=15, choices=Category, default="sell")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
     address = models.CharField(max_length=100)
     contact = models.IntegerField()
@@ -30,35 +55,35 @@ class Business(models.Model):
     def __str__(self):
         return self.name
 
-class healthservices(models.Model):
-    healthservices = models.CharField(max_length=100)
+class medicalservices(models.Model):
+    medicalservices = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.healthservices
+        return self.medicalservices
 
-    def save_healthservices(self):
+    def save_medicalservices(self):
         self.save()
 
     @classmethod
-    def delete_healthservices(cls, healthservices):
-        cls.objects.filter(healthservices=healthservices).delete()
+    def delete_medicalservices(cls, medicalservices):
+        cls.objects.filter(medicalservices=medicalservices).delete()
 
 
-class Health(models.Model):
-    photo = models.ImageField(upload_to='health/')
+class Medical(models.Model):
+    m_photo = models.ImageField(upload_to='medical/')
     name = models.CharField(max_length=100)
     email = models.EmailField()
     contact = models.IntegerField()
     address = models.CharField(max_length=100)
-    healthservices = models.ManyToManyField(healthservices)
+    medicalservices = models.ManyToManyField(medicalservices)
 
     def __str__(self):
         return self.name
 
 
-class Security(models.Model):
-   
+class Fund(models.Model):
     name = models.CharField(max_length=100)
+    amount = models.IntegerField()
     email = models.EmailField()
     contact = models.IntegerField()
     address = models.CharField(max_length=100)
@@ -66,26 +91,49 @@ class Security(models.Model):
     def __str__(self):
         return self.name
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=150)
-    image = models.ImageField(upload_to='post/')
-    post = HTMLField()
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = models.ImageField(upload_to='profiles/',null=True)
+    contact = models.CharField(max_length=12)
+    service = models.CharField(max_length=15, choices=Service, default="Driver")
+    email = models.EmailField(null=True)
+    bio = HTMLField(null=True)
   
-    post_date = models.DateTimeField(auto_now_add=True)
+    
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-    def __str__(self):
-        return self.title
+        post_save.connect(create_user_profile, sender=User)
+
 
     @classmethod
-    def search_post(cls, search_term):
-        blogs = cls.objects.filter(title__icontains=search_term)
-        return blogs
+    def get_profile(cls):
+        profile = Profile.objects.all()
 
+        return profile
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    def delete_profile(self):
+         self.delete()
+
+    def save_profile(self):
+        self.save()
+
+    class Meta:
+        ordering = ['user']
+
+    def __str__(self):
+        return self.username
 
 class Comment(models.Model):
     comment = models.CharField(max_length=300)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    cloth = models.ForeignKey(Cloth, on_delete=models.CASCADE)
+    motive = models.ForeignKey(Motivation, on_delete=models.CASCADE)
+
 
