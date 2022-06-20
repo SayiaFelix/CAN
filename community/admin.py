3ec1317dbce6d0fe736c98ef2723c8069a02fa24
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-20 15:59:42 +0300

    installing pollow and setting up db

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Cloth)
+admin.site.register(Profile)
+admin.site.register(Medical)
+admin.site.register(medicalservices)
+admin.site.register(Motivation)
+admin.site.register(Fund)
+admin.site.register(Comment)
