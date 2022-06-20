f95917c84e05bb389465541e5db07be9c9dd9a98
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-20 16:21:43 +0300

    adding profile template

---
+++ community/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+# class MotivationForm(forms.ModelForm):
+#     class Meta:
+#         model = Motivation
+#         # exclude = ['neighbourhood']
+        
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class ClothForm(forms.ModelForm):
+    class Meta:
+        model = Cloth
+        exclude = ['owner']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['username', 'cloth','motive']
+
--- community/views.py
+++ community/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ObjectDoesNotExist
 from .models import *
-# from .forms import *
+from .forms import *
 import datetime as datetime
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -12,3 +12,28 @@ from django.urls import reverse
 
 def services(request):
     return render(request,"services.html")
+
+
+def user_profile(request,profile_id):
+     current_user = request.user
+     try:
+      profile = Profile.objects.get(user=current_user)
+
+     except Profile.DoesNotExist:
+      profile = None
+     return render(request,"profile/profile.html",{"profile":profile})
+
+
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('sercices')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/update_profile.html', {"form": form})
