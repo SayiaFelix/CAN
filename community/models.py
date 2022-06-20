c977f9e60067c904900e3d06d8bf3b0ecfd6f162
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-06-20 14:39:11 +0300

    designing models

--- community/models.py
+++ community/models.py
@@ -3,55 +3,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 import datetime as dt
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 
 # Create your models here.
-class Hood(models.Model):
-    hood_name = models.CharField(max_length = 100)
-    hood_photo = models.ImageField(upload_to='hood/')
-    occupants_count = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.hood_name
-
-    def create_hood(self):
-        self.save()
-        
-    @classmethod
-    def delete_hood(cls, hood_name):
-        cls.objects.filter(hood_name=hood_name).delete()
-
-    @classmethod
-    def find_hood(cls, search_term):
-        search_results = cls.objects.filter(hood_name__icontains = search_term)
-        return search_results
-
-    def update_hood(self, hood_name, occupants_count):
-        self.hood_name = hood_name
-        self.occupants_count = occupants_count
-        self.save()
-
-class Information(models.Model):
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
-    info_photo = models.ImageField(upload_to='info/')
-    title = models.CharField(max_length=100)
-    information = HTMLField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Motivation(models.Model):
+    name = models.CharField(max_length=100)
+    motiv_photo = models.ImageField(upload_to='motive/')
+    motivation = HTMLField()
+    target = models.CharField(max_length=100)
     post_date = models.DateTimeField(auto_now_add=True)
 
-    def search_posts(cls, search_term):
-        blogs = cls.objects.filter(title__icontains=search_term)
-        return blogs
-   
+
     def __str__(self):
-        return self.title
+        return self.name
 
 class Business(models.Model):
     b_photo = models.ImageField(upload_to='bussiness/')
     description = HTMLField()
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
@@ -77,7 +46,6 @@ class healthservices(models.Model):
 
 class Health(models.Model):
     photo = models.ImageField(upload_to='health/')
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     contact = models.IntegerField()
@@ -89,7 +57,7 @@ class Health(models.Model):
 
 
 class Security(models.Model):
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+   
     name = models.CharField(max_length=100)
     email = models.EmailField()
     contact = models.IntegerField()
@@ -98,51 +66,13 @@ class Security(models.Model):
     def __str__(self):
         return self.name
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
-    profile_photo = models.ImageField(upload_to='profiles/',null=True)
-    contact = models.CharField(max_length=12)
-    email = models.EmailField(null=True)
-    bio = HTMLField(null=True)
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE,null=True)
-    
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-        post_save.connect(create_user_profile, sender=User)
-
-
-    @classmethod
-    def get_profile(cls):
-        profile = Profile.objects.all()
-
-        return profile
-
-    @classmethod
-    def search_profile(cls, name):
-        profile = Profile.objects.filter(user__username__icontains = name)
-        return profile
-
-    def delete_profile(self):
-         self.delete()
-
-    def save_profile(self):
-        self.save()
-
-    class Meta:
-        ordering = ['user']
-
-    def __str__(self):
-        return self.username
-
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     image = models.ImageField(upload_to='post/')
     post = HTMLField()
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    neighbourhood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+  
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
